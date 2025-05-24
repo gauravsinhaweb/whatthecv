da946e235311bdecfd7080966eeb0af4ade9455e
GAURAV SINHA <sinhagaurav.me@gmail.com>
2025-05-24 23:50:17 +0530

    update: build fail for vite supabase config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       react: 'preact/compat',
       'react-dom': 'preact/compat',
       'react-dom/test-utils': 'preact/test-utils', // Optional, for testing
